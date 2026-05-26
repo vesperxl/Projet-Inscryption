3379422246c2fb2822de5841e6e750b065869357
Despres Lilian <lilian.despres@etu.unistra.fr>
2026-05-26 17:18:33 +0200

    Ajout du start dans display

--- src/Display/Display.java
+++ src/Display/Display.java
@@ -1,4 +1,9 @@
 package Display;
 
 public class Display {
+    public void start(){
+        System.out.println("/////////////////////////////////////////");
+        System.out.println("///////////////INSCRYPTION///////////////");
+        System.out.println("/////////////////////////////////////////");
+    }
 }
