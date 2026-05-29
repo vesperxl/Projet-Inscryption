4a44270119fbb8327cc50b5e3ce5c0fd095e4d56
Despres Lilian <lilian.despres@etu.unistra.fr>
2026-05-29 14:01:30 +0200

    Début Battle

--- src/gameplay/Battle.java
+++ src/gameplay/Battle.java
@@ -1,4 +1,38 @@
 package gameplay;
 
-public class Battle {
+import Card.Obstacles.Fir;
+import Card.Obstacles.Rock;
+
+import java.util.Random;
+
+public class Battle
+{
+    private Board board;
+    private void initialize (){
+        Random rand = new Random();
+
+        for (int i = 0; i <= 3; i++){
+
+            int chance = rand.nextInt(10);
+
+            if (chance == 0)
+            {
+                chance = rand.nextInt(2);
+                if (chance == 0)
+                {
+                    board.placePlayerCard(new Rock(), i);
+                }
+                else
+                {
+                    board.placePlayerCard(new Fir(), i);
+                }
+            }
+        }
+    }
+
+    private void attack()
+    {
+
+    }
+
 }
