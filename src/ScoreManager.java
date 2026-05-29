465db5ceb805c00ca61c898fec16b8a9712518fc
Louis Stenger <stengerl@unistra.fr>
2026-05-29 14:06:27 +0200

    ajout classe ScoreManager et commencement classe Game

---
+++ src/ScoreManager.java
@@ -0,0 +1,26 @@
+public class ScoreManager {
+    private int _score;
+
+    public ScoreManager(){
+        _score = 0;
+    }
+
+    public void addPlayerPoint(int points){
+        _score += points;
+    }
+    public void addEnemyPoint(int points){
+        _score -= points;
+    }
+
+    public int getScore(){
+        return _score;
+    }
+
+    public boolean playerVictory(){
+        return _score >= 5;
+    }
+
+    public boolean enemyVictory(){
+        return _score <= -5;
+    }
+}
