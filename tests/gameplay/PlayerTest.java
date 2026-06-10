dd03d68d738d2536ef20354964ad0e8ab7638cd5
Louis Stenger <stengerl@unistra.fr>
2026-06-10 10:47:14 +0200

    changemnt test

--- tests/gameplay/PlayerTest.java
+++ tests/gameplay/PlayerTest.java
@@ -20,21 +20,17 @@ public class PlayerTest {
     
     @Test
     public void testAjoutNouvellesCartesPiocheFinDeuxiemePartie() {
-        // Test de "l'ajout de nouvelles cartes dans la pioche à la fin de la deuxième partie"
         Player player = new Player();
         
-        // On simule la fin de la deuxième partie
-        player.newGame(); // Début partie 1
-        player.newGame(); // Début partie 2
+        player.newGame();
+        player.newGame();
         
         int sizeBefore = player.getInitialDeck().getSizeDeck();
         
-        // Ajout d'une carte à la pioche initiale comme dans GameDisplay.cardChoice()
         player.getInitialDeck().addCard(new Wolf());
         
         assertEquals(sizeBefore + 1, player.getInitialDeck().getSizeDeck());
         
-        // Début de la partie 3, le joueur pioche sa main (4 cartes)
         player.newGame();
         
         assertEquals(sizeBefore + 1 - 4, player.getDeck().getSizeDeck());
